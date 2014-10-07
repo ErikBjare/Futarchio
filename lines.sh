50ce24d6591c0e150cb9be3dbdda3a9efd730e50
Erik Bjäreholt <erik.bjareholt@gmail.com>
2014-10-07 10:06:37 +0200

    Refined lines.sh

--- lines.sh
+++ lines.sh
@@ -1,19 +1,26 @@
+#!/bin/bash
+
 cd src
 
 re_js="site/src/.*[^(\.min)]\.js$"
 re_html="site/src/[^/]*\.html$"
-re_go="\.go$"
-
-echo -n "Lines of JS: "
-cat `find | grep $re_js` | wc -l
+re_go="[^(_test)]\.go$"
+re_go_tests="_test\.go$"
 
 echo -n "Lines of HTML: "
 cat `find | grep $re_html` | wc -l
 
-echo -n "Lines of Go: "
-cat `find | grep $re_go` | wc -l
+echo -n "Lines of JS: "
+cat `find | grep $re_js` | wc -l
 
-echo "Verbose:"
-echo "JS: $(echo `find | grep $re_js`)"
-echo "HTML: $(echo `find | grep $re_html`)"
-echo "Go: $(echo `find | grep $re_go`)"
+echo -n "Lines of Go (excluding tests): "
+cat `find | grep $re_go` | wc -l
+echo -n "Lines of Go tests: "
+cat `find | grep $re_go_tests` | wc -l
+
+#echo ""
+#echo "Verbose:"
+#echo "JS: $(echo `find | grep $re_js`)"
+#echo "HTML: $(echo `find | grep $re_html`)"
+#echo "Go: $(echo `find | grep $re_go`)"
+#echo "Go: $(echo `find | grep $re_go_tests`)"
