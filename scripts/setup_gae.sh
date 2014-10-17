094770648719cc7c28a9948f2d4a78b3df71e463
Erik Bjäreholt <erik.bjareholt@gmail.com>
2014-10-17 19:19:53 +0200

    Changed GAE download url

--- scripts/setup_gae.sh
+++ scripts/setup_gae.sh
@@ -3,7 +3,7 @@
 # TODO: Remove following line when script works
 echo $(pwd)
 
-wget https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.12.zip -O go_appengine.zip -nv
+wget https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.13.zip -O go_appengine.zip -nv
 unzip -q go_appengine.zip
 
 IFS=':' read -ra GOPKGS <<< "$GOPATH"
