1847482d7fad97dd8fb20a2213bb7a88b14c47eb
Erik Bjäreholt <erik.bjareholt@gmail.com>
2014-11-09 16:10:40 +0100

    Removed excess symlinks (I think)

--- scripts/setup_gae.sh
+++ scripts/setup_gae.sh
@@ -5,7 +5,3 @@ echo $(pwd)
 
 wget https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.14.zip -O go_appengine.zip -nv
 unzip -q go_appengine.zip
-
-IFS=':' read -ra GOPKGS <<< "$GOPATH"
-ln -s "$(pwd)/go_appengine/goroot/src/pkg/appengine" "${GOPKGS[0]}/src/appengine"
-ln -s "$(pwd)/go_appengine/goroot/src/pkg/appengine_internal" "${GOPKGS[0]}/src/appengine_internal"
