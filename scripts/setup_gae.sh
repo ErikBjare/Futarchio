277b5495989ec28a790005104d305f601389e50a
Erik Bjäreholt <erik.bjareholt@gmail.com>
2014-11-09 16:11:58 +0100

    Update setup_gae.sh

--- scripts/setup_gae.sh
+++ scripts/setup_gae.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# TODO: Remove following line when script works
-echo $(pwd)
-
 wget https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.14.zip -O go_appengine.zip -nv
 unzip -q go_appengine.zip
