fbb4d421a0fab46e2d6b1f66b5cef5d5e0fc3286
Erik Bjäreholt <erik.bjareholt@gmail.com>
2014-11-20 16:51:15 +0100

    Fixed GAE download url, cleaned up HTML in index.html, updated wikiref

--- scripts/setup_gae.sh
+++ scripts/setup_gae.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.14.zip -O go_appengine.zip -nv
+wget https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.15.zip -O go_appengine.zip -nv
 unzip -q go_appengine.zip
