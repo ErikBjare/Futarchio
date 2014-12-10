6b1aff9353a633bae819ab3debc807599ff4f0f5
Erik Bjäreholt <erik.bjareholt@gmail.com>
2014-12-10 10:24:37 +0100

    Upgraded to Meteor 1.0.1, removed angular remains

--- client/main.js
+++ client/main.js
@@ -1,11 +1,4 @@
 Accounts.ui.config({
-    requestPermissions: {
-      facebook: ['user_likes'],
-      github: ['user', 'repo']
-    },
-    requestOfflineToken: {
-      google: true
-    },
     passwordSignupFields: 'USERNAME_AND_EMAIL'
 });
 
