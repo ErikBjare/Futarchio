889e4e3d33b13a57b8f3076d1116874543eddebf
Erik Bjäreholt <erik.bjareholt@gmail.com>
2014-12-11 16:53:19 +0100

    Updated lines.py script

--- scripts/lines.py
+++ scripts/lines.py
@@ -5,8 +5,9 @@ import os
 import subprocess
 import argparse
 
-ROOT_DIR = "../"
-SRC_DIR = ROOT_DIR + "src"
+PWD = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(PWD)
+SRC_DIR = ROOT_DIR + "/src"
 
 def recurse_dir(folder):
     files = [folder + "/" + f for f in os.listdir(folder)]
@@ -19,7 +20,7 @@ def recurse_dir(folder):
         files.extend(new_files)
     return files
 
-def count_lines(folder, pattern):
+def count_lines(folder, pattern, verbose=True):
     print("Folder: {}, pattern: {}".format(folder, pattern))
     files = recurse_dir(folder)
     a = [re.fullmatch(pattern, f) for f in files]
@@ -28,41 +29,49 @@ def count_lines(folder, pattern):
 
     cmd = ["wc"]
     cmd.extend(c)
-    subprocess.call(cmd)
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    out, err = p.communicate()
 
-def list_all():
+    out = out.decode("utf-8")
+    if verbose:
+        print(out)
+    else:
+        print(out.split("\n")[-2])
+
+def list_all(verbose=False):
     # HTML
     print("HTML")
-    count_lines(ROOT_DIR + "client", ".*\.html$")
+    count_lines(ROOT_DIR + "/client", ".*\.html$", verbose)
 
     # JS
     print("\nJS [lib, client, server]")
-    count_lines(ROOT_DIR + "lib", ".+\\.js$")
-    count_lines(ROOT_DIR + "server", ".+\\.js$")
-    count_lines(ROOT_DIR + "client", ".+\\.js$")
+    count_lines(ROOT_DIR + "/lib", ".+\\.js$")
+    count_lines(ROOT_DIR + "/server", ".+\\.js$", verbose)
+    count_lines(ROOT_DIR + "/client", ".+\\.js$", verbose)
 
     # JS Tests
     print("\nJS Tests")
-    count_lines(ROOT_DIR + "tests", ".+\\.js")
+    count_lines(ROOT_DIR + "/tests", ".+\\.js", verbose)
 
 
     # Selenium tests
     # print("\nSelenium tests (Python)")
-    # count_lines(ROOT_DIR + "tests", ".+\\.py$")
+    # count_lines(ROOT_DIR + "tests", ".+\\.py$", verbose)
 
     # Misc, Wiki etc.
     print("\nWiki")
-    count_lines(ROOT_DIR + "wiki", ".+\\.md$")
+    count_lines(ROOT_DIR + "/wiki", ".+\\.md$", verbose)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="List the files matching a certain pattern and the number of contained lines, words and characters.")
     parser.add_argument("-p", "--pattern", help="pattern to use when looking for files")
     parser.add_argument("-d", "--dir", help="directory to look for files in")
+    parser.add_argument("-v", "--verbose", help="lists all matched files", action='store_true')
 
     args = parser.parse_args()
     if not args.pattern:
-        list_all()
+        list_all(verbose=args.verbose)
     elif args.pattern and args.dir:
         count_lines(args.dir, args.pattern)
     else:
