949b2c86dc1adec1314e66eb9096761f5fd0b72f
Erik Bjäreholt <erik.bjareholt@gmail.com>
2014-12-12 23:45:03 +0100

    Added meteor-toastr

---
+++ client/startup.js
@@ -0,0 +1,19 @@
+Meteor.startup(function() {
+    console.info("Started...");
+
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    };
+});
